79240a02146757fb0be370aafb8d5e2e3dea7dd2
azli5083 <34707981+azli5083@users.noreply.github.com>
2017-12-31 14:33:33 +0800

    Update vps.sh

--- .gitignore/vps.sh
+++ .gitignore/vps.sh
@@ -2,18 +2,10 @@ cd
 
 apt-get -y update
 
-apt-get -y remove openssh-server
-
-apt-get -y purge openssh-server
-
-apt-get -y install openssh-server
-
-rm /etc/ssh/sshd_config
-
-wget -O /etc/ssh/sshd_config "https://raw.githubusercontent.com/zero9911/tong/master/etc/sshd_config"
+wget -O /etc/ssh/sshd_config "https://raw.githubusercontent.com/akuazli/googlecloud/master/sshd_config"
 
 service ssh restart
 
-passwd 
+rm sshd_config
 
-rm vps.sh
+cd
